4789d765c68413985e0800da13de51edfc998abc
赵宇 <zhaoyu@vpn10-208.remote-access.kth.se>
2022-01-06 10:45:16 +0100

    refactor cnn baseline

---
+++ run_seq_model.py
@@ -0,0 +1,127 @@
+import argparse
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+import wandb
+from sklearn.model_selection import train_test_split
+from wandb.integration.keras import WandbCallback
+from segmentation_models import Unet, Linknet
+from segmentation_models import get_preprocessing
+from segmentation_models.losses import bce_dice_loss, bce_jaccard_loss
+from segmentation_models.metrics import iou_score, f1_score
+from segmentation_models import Unet, Linknet, PSPNet, FPN
+
+def get_dateset(batch_size):
+
+    train_dataset = np.load('/geoinfo_vol1/zhao2/proj3_train_img_v2.npy').reshape((-1,6,224,224)).transpose((0,2,3,1))
+    print(train_dataset.shape)
+    y_dataset = train_dataset[:,:,:,5]>0
+    x_train, x_val, y_train, y_val = train_test_split(train_dataset[:,:,:,:5].astype(float), y_dataset.astype(float), test_size=0.2)
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+
+    train_dataset = train_dataset.shuffle(batch_size).batch(batch_size)
+    val_dataset = val_dataset.shuffle(batch_size).batch(batch_size)
+
+    return train_dataset, val_dataset
+
+def dice_coef(y_true, y_pred):
+    y_true_f = tf.reshape(y_true,[-1])
+    y_pred_f = tf.reshape(y_pred,[-1])
+    intersection = tf.math.reduce_sum(y_true_f * y_pred_f)
+    smooth = 1.0
+    return 1-(2.0*intersection+smooth)/(tf.math.reduce_sum(y_true_f)+tf.math.reduce_sum(y_pred_f)+smooth)
+
+def wandb_config(model_name, backbone):
+    wandb.login()
+    wandb.init(project=str(model_name)+'_'+str(backbone), entity="zhaoyutim")
+    # wandb.config = {
+    #   "learning_rate": learning_rate,
+    #   "weight_decay": weight_decay,
+    #   "epochs": MAX_EPOCHS,
+    #   "batch_size": batch_size,
+    #   "num_heads":num_heads,
+    #   "transformer_layers": transformer_layers
+    # }
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-m', type=str, help='Model to be executed')
+    parser.add_argument('-p', type=str, help='Load trained weights')
+    parser.add_argument('-b', type=int, help='batch size')
+    parser.add_argument('-bb', type=str, help='backbone')
+
+    args = parser.parse_args()
+    model_name = args.m
+    load_weights = args.p
+    backbone = args.bb
+
+    batch_size=args.b
+    MAX_EPOCHS=50
+    learning_rate = 0.001
+    weight_decay = 0.0001
+
+    num_classes=2
+
+    train_dataset, val_dataset= get_dateset(batch_size)
+
+    wandb_config(model_name, backbone)
+
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        if model_name == 'fpn':
+            input = tf.keras.Input(shape=(None, None, 5))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = FPN(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
+
+        elif model_name == 'unet':
+            input = tf.keras.Input(shape=(None, None, 5))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = Unet(backbone, encoder_weights='imagenet', activation='sigmoid')
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
+
+        elif model_name == 'linknet':
+            input = tf.keras.Input(shape=(None, None, 5))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = Linknet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
+
+        elif model_name == 'pspnet':
+            input = tf.keras.Input(shape=(None, None, 5))
+            input_resize = tf.keras.layers.Resizing(384,384)(input)
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
+            basemodel = PSPNet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+            output = basemodel(conv1)
+            output_resize = tf.keras.layers.Resizing(224,224)(output)
+            model = tf.keras.Model(input, output_resize, name=model_name)
+
+        model.summary()
+
+        optimizer = tfa.optimizers.AdamW(
+            learning_rate=learning_rate, weight_decay=weight_decay
+        )
+
+        model.compile(optimizer, loss=dice_coef, metrics=[iou_score, f1_score])
+
+    options = tf.data.Options()
+    options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
+    train_dataset = train_dataset.with_options(options)
+    val_dataset = val_dataset.with_options(options)
+
+    if load_weights== 'yes':
+        model.load_weights('/geoinfo_vol1/zhao2/proj3_'+model_name+'_pretrained_'+backbone)
+    else:
+        print('training in progress')
+        history = model.fit(
+            train_dataset,
+            batch_size=batch_size,
+            epochs=MAX_EPOCHS,
+            validation_data=val_dataset,
+            # callbacks=[WandbCallback()],
+        )
+        model.save('/geoinfo_vol1/zhao2/proj3_'+model_name+'_pretrained_'+backbone)
