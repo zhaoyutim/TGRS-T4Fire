983e30a112730004c5e0f7ec5203cb9bee37f91e
赵宇 <zhaoyu@vpn10-208.remote-access.kth.se>
2022-01-06 10:48:46 +0100

    refactor cnn baseline

--- run_cnn_model.py
+++ run_cnn_model.py
@@ -6,42 +6,36 @@ import tensorflow_addons as tfa
 import wandb
 from sklearn.model_selection import train_test_split
 from wandb.integration.keras import WandbCallback
+from segmentation_models import Unet, Linknet
+from segmentation_models import get_preprocessing
+from segmentation_models.losses import bce_dice_loss, bce_jaccard_loss
+from segmentation_models.metrics import iou_score, f1_score
+from segmentation_models import Unet, Linknet, PSPNet, FPN
 
-from model.gru.gru_model import GRUModel
-from model.vit_keras import vit
-def get_dateset(window_size, batch_size):
-    x_dataset = np.load('/geoinfo_vol1/zhao2/proj3_train_v2_w'+str(window_size)+'.npy')
-    y_dataset = np.zeros((x_dataset.shape[0],x_dataset.shape[1],2))
-    y_dataset[: ,:, 0] = x_dataset[:, :, pow(window_size,2)*5] == 0
-    y_dataset[:, :, 1] = x_dataset[:, :, pow(window_size,2)*5] > 0
+def get_dateset(batch_size):
 
-    x_train, x_val, y_train, y_val = train_test_split(x_dataset[:,:,:pow(window_size,2)*5], y_dataset, test_size=0.2)
+    train_dataset = np.load('/geoinfo_vol1/zhao2/proj3_train_img_v2.npy').reshape((-1,6,224,224)).transpose((0,2,3,1))
+    print(train_dataset.shape)
+    y_dataset = train_dataset[:,:,:,5]>0
+    x_train, x_val, y_train, y_val = train_test_split(train_dataset[:,:,:,:5].astype(float), y_dataset.astype(float), test_size=0.2)
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
 
-    def make_generator(inputs, labels):
-        def _generator():
-            for input, label in zip(inputs, labels):
-                yield input, label
+    train_dataset = train_dataset.shuffle(batch_size).batch(batch_size)
+    val_dataset = val_dataset.shuffle(batch_size).batch(batch_size)
 
-        return _generator
+    return train_dataset, val_dataset
 
+def dice_coef(y_true, y_pred):
+    y_true_f = tf.reshape(y_true,[-1])
+    y_pred_f = tf.reshape(y_pred,[-1])
+    intersection = tf.math.reduce_sum(y_true_f * y_pred_f)
+    smooth = 1.0
+    return 1-(2.0*intersection+smooth)/(tf.math.reduce_sum(y_true_f)+tf.math.reduce_sum(y_pred_f)+smooth)
 
-    train_dataset = tf.data.Dataset.from_generator(make_generator(x_train, y_train),
-                                                   (tf.float32, tf.int16))
-    val_dataset = tf.data.Dataset.from_generator(make_generator(x_val, y_val),
-                                                 (tf.float32, tf.int16))
-
-    train_dataset = train_dataset.shuffle(batch_size).repeat(MAX_EPOCHS).batch(batch_size)
-    val_dataset = val_dataset.shuffle(batch_size).repeat(MAX_EPOCHS).batch(batch_size)
-
-    steps_per_epoch = x_train.shape[0]//batch_size
-    validation_steps = x_val.shape[0]//batch_size
-
-    return train_dataset, val_dataset, steps_per_epoch, validation_steps
-
-
-def wandb_config(window_size, model_name):
+def wandb_config(model_name, backbone):
     wandb.login()
-    wandb.init(project="tokenized_window_size"+ str(window_size) +str(model_name), entity="zhaoyutim")
+    wandb.init(project=str(model_name)+'_'+str(backbone), entity="zhaoyutim")
     # wandb.config = {
     #   "learning_rate": learning_rate,
     #   "weight_decay": weight_decay,
@@ -54,61 +48,57 @@ def wandb_config(window_size, model_name):
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-m', type=str, help='Model to be executed')
-    parser.add_argument('-w', type=int, help='Window size')
     parser.add_argument('-p', type=str, help='Load trained weights')
     parser.add_argument('-b', type=int, help='batch size')
+    parser.add_argument('-bb', type=str, help='backbone')
 
     args = parser.parse_args()
     model_name = args.m
     load_weights = args.p
-    window_size = args.w
+    backbone = args.bb
 
     batch_size=args.b
-    MAX_EPOCHS = 50
+    MAX_EPOCHS=50
     learning_rate = 0.001
     weight_decay = 0.0001
 
     num_classes=2
-    input_shape=(10,pow(window_size,2)*5)
 
-    train_dataset, val_dataset, steps_per_epoch, validation_steps = get_dateset(window_size, batch_size)
+    train_dataset, val_dataset= get_dateset(batch_size)
 
-    wandb_config(window_size, model_name)
+    wandb_config(model_name, backbone)
 
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
-        if model_name == 'vit_small':
-            model = vit.vit_small(
-                input_shape=input_shape,
-                classes=num_classes,
-                activation='sigmoid',
-                pretrained=True,
-                include_top=True,
-                pretrained_top=True
-            )
-        elif model_name=='vit_tiny':
-            model = vit.vit_tiny(
-                input_shape=input_shape,
-                classes=num_classes,
-                activation='sigmoid',
-                pretrained=True,
-                include_top=True,
-                pretrained_top=True
-            )
-        elif model_name == 'gru20':
-            gru = GRUModel(input_shape, num_classes)
-            model = gru.get_model_10_layers(input_shape, num_classes)
-        elif model_name=='vit_base':
-            model = vit.vit_base(
-                input_shape=input_shape,
-                classes=num_classes,
-                activation='sigmoid',
-                pretrained=False,
-                include_top=True,
-                pretrained_top=True
-            )
-        else:
-            raise('no suport model')
+        if model_name == 'fpn':
+            input = tf.keras.Input(shape=(None, None, 5))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = FPN(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
+
+        elif model_name == 'unet':
+            input = tf.keras.Input(shape=(None, None, 5))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = Unet(backbone, encoder_weights='imagenet', activation='sigmoid')
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
+
+        elif model_name == 'linknet':
+            input = tf.keras.Input(shape=(None, None, 5))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = Linknet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
+
+        elif model_name == 'pspnet':
+            input = tf.keras.Input(shape=(None, None, 5))
+            input_resize = tf.keras.layers.Resizing(384,384)(input)
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
+            basemodel = PSPNet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+            output = basemodel(conv1)
+            output_resize = tf.keras.layers.Resizing(224,224)(output)
+            model = tf.keras.Model(input, output_resize, name=model_name)
 
         model.summary()
 
@@ -116,13 +106,7 @@ if __name__=='__main__':
             learning_rate=learning_rate, weight_decay=weight_decay
         )
 
-        model.compile(
-            optimizer=optimizer,
-            loss=tfa.losses.SigmoidFocalCrossEntropy(from_logits=False),
-            metrics=[
-                tf.keras.metrics.CategoricalAccuracy(name="accuracy")
-            ],
-        )
+        model.compile(optimizer, loss=dice_coef, metrics=[iou_score, f1_score])
 
     options = tf.data.Options()
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
@@ -130,16 +114,14 @@ if __name__=='__main__':
     val_dataset = val_dataset.with_options(options)
 
     if load_weights== 'yes':
-        model.load_weights('/geoinfo_vol1/zhao2/proj3_' + model_name + 'w' + str(window_size) + '_nopretrained')
+        model.load_weights('/geoinfo_vol1/zhao2/proj3_'+model_name+'_pretrained_'+backbone)
     else:
         print('training in progress')
         history = model.fit(
-            x=train_dataset,
+            train_dataset,
             batch_size=batch_size,
-            steps_per_epoch=steps_per_epoch,
-            validation_data=val_dataset,
-            validation_steps=validation_steps,
             epochs=MAX_EPOCHS,
-            callbacks=[WandbCallback()],
+            validation_data=val_dataset,
+            # callbacks=[WandbCallback()],
         )
-        model.save('/geoinfo_vol1/zhao2/proj3_'+model_name+'w' + str(window_size) + '_nopretrained')
+        model.save('/geoinfo_vol1/zhao2/proj3_'+model_name+'_pretrained_'+backbone)
