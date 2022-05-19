16cdbdc487ec2c1b0e6c933058aae20baf52eb8d
赵宇 <zhaoyu@vpn37-100.remote-access.kth.se>
2022-05-19 09:22:19 +0200

    first commit

--- svg_processor.py
+++ svg_processor.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
-path = '/Users/zhaoyu/PycharmProjects/T4Fire/svg/afdes1.csv'
+path = '/Users/zhaoyu/PycharmProjects/T4Fire/svg/afdes2.csv'
 if __name__=='__main__':
     df = pd.read_csv(path)
     df['b4_avg']=df['b4'].rolling(1).sum()
     df['b5_avg']=df['b5'].rolling(1).sum()
+    df['Date']=pd.to_datetime(df['system:time_start']).dt.strftime('%m-%d')
     print(df.head(5))
-    df.plot(x="system:time_start", y=['b4', 'b5'], figsize=(10,5))
-    plt.savefig('afdes1.svg', bbox_inches='tight')
+    plt.rc('xtick', labelsize=12)
+    plt.rc('ytick', labelsize=12)
+    df.plot(x="Date", y=['b4', 'b5'], figsize=(10,5))
+    plt.savefig('afdes2.svg', bbox_inches='tight')
     plt.show()
     # plt.imshow(path)
     # plt.show()
